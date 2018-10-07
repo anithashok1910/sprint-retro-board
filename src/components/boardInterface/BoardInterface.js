9d19c21a8e8835f0de7c15438de3bf3933701470
Anith Ashok <anith.ashok1910@gmail.com>
2018-10-07 22:52:53 +0530

    add cards for the two options

--- src/components/boardInterface/BoardInterface.js
+++ src/components/boardInterface/BoardInterface.js
@@ -1,52 +1,110 @@
-import React, { Component } from 'react'
-import Paper from '@material-ui/core/Paper';
+import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 
-
-class BoardInterface extends Component{
-
-
-    render = () => {
-
-        let styles = {
-            margin: "10%",
-            padding:"10%",
-            height:"65%",
-            textAlign: 'center',
-        };
-        return (
-            <div style = {{backgroundColor: '#eeeeee'}}>
-                <Grid container spacing={24}>
-                    <Grid item xs={12} sm={6}>
-                         <Paper style = {styles}>
-                         <TextField
-                            id="boardID"
-                            label="Board Id"
-                            
-                            value=""
-                            // onChange={this.handleChange('name')}
-                            margin="normal"
-                            />
-                            <Typography variant="headline" component="h3">
-                                Join Existing Board
-                            </Typography>
-                        </Paper>
+//https://github.com/mui-org/material-ui/blob/master/docs/src/pages/page-layout-examples/pricing/Pricing.js
+//https://material-ui.com/page-layout-examples/pricing/
+const boardInterfaceStyles = theme => ({
+    '@global': {
+        body: {
+          backgroundColor: theme.palette.common.white,
+        },
+      },
+      layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+          width: 900,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        },
+      },
+      heroContent: {
+        maxWidth: 600,
+        margin: '0 auto',
+        padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+      },
+      cardHeader: {
+        backgroundColor: theme.palette.grey[200],
+      },
+      cardActions: {
+        [theme.breakpoints.up('sm')]: {
+          paddingBottom: theme.spacing.unit * 2,
+        },
+      }
+});
+const BoardInterface = props =>{
+    const {classes} = props;
+    return (
+            <CssBaseline>
+                <main className={classes.layout}>
+                    {/* Hero unit */}
+                        <div className={classes.heroContent}>
+                        <Typography variant="display3" align="center" color="textPrimary" gutterBottom>
+                            Agile Boards
+                        </Typography>
+                        <Typography variant="title" align="center" color="textSecondary" component="p">
+                            Quickly build an effective Agile Board for your agile team from a variety of Agile boards options.
+                            It&apos;s built for enhancing team work and productivity.
+                        </Typography>
+                        </div>
+                    {/* End hero unit */}
+                        <Grid container spacing={40} alignItems="flex-end">
+                            <Grid item xs={12} sm={6} md={6}>
+                                <Card>
+                                    <CardHeader
+                                    title="JOIN"
+                                    subheader="Join existing Agile Board"
+                                    titleTypographyProps={{ align: 'center' }}
+                                    subheaderTypographyProps={{ align: 'center' }}
+                                    className={classes.cardHeader}
+                                    />
+                                    <CardContent>
+                                        <Typography variant="subheading" align="center" key={"line1"}>
+                                            {"Join the board from the existing started board invited by your team"}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions className={classes.cardActions}>
+                                        <Button  fullWidth variant="contained" color="primary">
+                                            Join now
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={6}>
+                                <Card>
+                                    <CardHeader
+                                    title="CREATE"
+                                    subheader="Join existing Agile Board"
+                                    titleTypographyProps={{ align: 'center' }}
+                                    subheaderTypographyProps={{ align: 'center' }}
+                                    className={classes.cardHeader}
+                                    />
+                                    <CardContent>
+                                        <Typography variant="subheading" align="center" key={"line1"}>
+                                            {"Join the board from the existing started board invited by your team"}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions className={classes.cardActions}>
+                                        <Button  fullWidth variant="contained" color="primary">
+                                            Get Started
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper style = {styles}>
-                            <Typography variant="headline" component="h3">
-                                Create new Board
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </div>
-        )
-    } 
-}
+                </main>
+            </CssBaseline>
+           
+        );    
+};
 
-export default BoardInterface;
-
-//This place will act as a place holder for all the Board works
+export default withStyles(boardInterfaceStyles)(BoardInterface);
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,38 @@
 import React from "react";
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-const Navbar = () =>{
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
+const navBarStyles = theme => ({
+    '@global': {
+        body: {
+          backgroundColor: theme.palette.common.white,
+        },
+      },
+    appBar:{
+        position: 'relative',
+    },
+    toolbarTitle:{
+        flex: 1,
+    }
+});
+
+const Navbar = (props) =>{
+
+    const {classes} = props;
     return(
-        <div>
-        <AppBar position="static">
+        <CssBaseline>
+        <AppBar position="static" color="default" className={classes.appBar}>
             <Toolbar>
-                <Typography variant="title" color="inherit">
-                Sprint Retro Board
+                <Typography variant="title" color="inherit" noWrap className={classes.toolbarTitle}>
+                    SkunkWorkz
                 </Typography>
             </Toolbar>
         </AppBar>
-        </div>
+        </CssBaseline>
     )
     
 }
-
-export default Navbar;
+export default withStyles(navBarStyles)(Navbar);
